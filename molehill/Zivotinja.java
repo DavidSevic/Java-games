8074f4b3fcd5982f2a8f8226b7c1c3a7c6f4f710
DavidSevic <93209409+DavidSevic@users.noreply.github.com>
2021-10-29 12:06:30 +0200

    Add files via upload

---
+++ molehill/Zivotinja.java
@@ -0,0 +1,19 @@
+package igra;
+
+
+public abstract class Zivotinja {
+	
+	protected Rupa rupa;
+	
+	public Zivotinja(Rupa rupa) {
+		super();
+		this.rupa = rupa;
+	}
+
+	public abstract void ispolji_udarena();
+	public abstract void ispolji_pobegla();
+
+	public abstract void iscrtaj(double procenat);
+	
+
+}
