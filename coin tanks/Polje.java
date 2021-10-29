6ed60817a33e5e1e75e29b32d86943a757c8d897
DavidSevic <93209409+DavidSevic@users.noreply.github.com>
2021-10-29 12:09:31 +0200

    Add files via upload

---
+++ coin tanks/Polje.java
@@ -0,0 +1,55 @@
+package lavirint;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import lavirint.Igra.Vrsta;
+
+public abstract class Polje extends Canvas {
+	
+	private Mreza mreza;
+	private int vrsta;
+	private int kolona;
+	
+	public Polje(Mreza mreza) {
+		super();
+		this.mreza = mreza;
+		
+		
+		addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if(mreza.rezim() == Vrsta.IZMENA)
+					mreza.izmeni_polje(pozicija());
+			}
+		});
+		
+	}
+	
+	public int[] pozicija() {
+		int niz[] = new int[2];
+		niz[0] = vrsta;
+		niz[1] = kolona;
+		return niz;
+	}
+	
+	public void postavi_poz(int i, int j) {
+		vrsta = i;
+		kolona = j;
+	}
+	
+	public Polje udaljeno(int i, int j) {
+		return mreza.nadji_udaljeno(vrsta, kolona, i, j);
+	}
+	
+	public Mreza getMreza() {
+		return mreza;
+	}
+	
+	public abstract boolean dozvoljeno(Figura figura);
+	
+
+}
---
+++ coin tanks/Trava.java
@@ -0,0 +1,22 @@
+package lavirint;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Trava extends Polje {
+	
+	public Trava(Mreza mreza) {
+		super(mreza);
+	}
+	
+	@Override
+	public boolean dozvoljeno(Figura figura) {
+		return true;
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		setBackground(Color.GREEN);
+	}
+	
+}
